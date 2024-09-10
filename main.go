8033a7a172d9b3b095d6e4474968d9352bf31b46
mmoulabbi <Mehdi.nada909@gmail.com>
2024-09-10 18:59:17 +0100

    testint commit by a file

--- main.go
+++ main.go
@@ -8,25 +8,31 @@ import (
 )
 
 func main() {
-	if len(os.Args) < 2 {
+
+    if len(os.Args)  != 1  {
 		fmt.Println("Usage: go run your-program.go data.txt")
 		return
 	}
-	filename := os.Args[1]
+
+    filename := os.Args[1]
 	nbrs, err := Mt.ReadFile(filename)
-	if err != nil {
+
+    if err != nil {
 		fmt.Println("Error reading file:", err)
 		return
 	}
+    
 	if len(nbrs) == 0 {
 		fmt.Println("No data to process.")
 		return
 	}
+
 	avg := Mt.Average(nbrs)
 	med := Mt.Median(nbrs)
 	hh := Mt.Variance(nbrs, avg)
 	stdDev :=  Mt.StandardDeviation(hh)
-	fmt.Printf("Average: %d\n", int(math.Round(avg)))
+	
+    fmt.Printf("Average: %d\n", int(math.Round(avg)))
 	fmt.Printf("Median: %d\n", int(math.Round(med)))
 	fmt.Printf("Variance: %d\n", int(math.Round(hh)))
 	fmt.Printf("Standard Deviation: %d\n", int(math.Round(stdDev)))
